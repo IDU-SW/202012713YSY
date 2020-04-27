51a1b27e0b58371225df32e1d47e3370ee0ca1d5
soyoung <dbthdud97@gmail.com>
2020-04-27 09:20:04 +0900

    soyoung

---
+++ router/book_router.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const books = require('../model/books');
+
+router.get('/books', showBookList);
+router.get('/Books/:bookId', showBookDetail);
+router.post('/books', addBook);
+
+module.exports = router;
+
+function showBookList(req, res) {
+    const bookList = books.getBookList();
+    const result = { data:bookList, count:bookList.length };
+    res.send(result);
+}
+
+
+// Async-await를 이용하기
+async function showBookDetail(req, res) {
+    try {
+        const bookId = req.params.bookId;
+        console.log('bookId : ', bookId);
+        const info = await books.getBookDetail(bookId);
+        res.send(info);
+    }
+    catch ( error ) {
+        console.log('Can not find, 404');
+        res.status(error.code).send({msg:error.msg});
+    }
+}
+
+
+// 새 영화 추가
+// POST 요청 분석 -> 바디 파서
+async function addBook(req, res) {
+    const title = req.body.title;
+
+    if (!title) {
+        res.status(400).send({error:'title 누락'});
+        return;
+    }
+
+    const director = req.body.director;
+    const year = parseInt(req.body.year);
+    const synopsis = req.body.synopsis;
+
+    try {
+        const result = await books.addBook(title, director, year, synopsis);
+        res.send({msg:'success', data:result});
+    }
+    catch ( error ) {
+        res.status(500).send(error.msg);
+    }
+}
